715b63f706e5f847d7fba097cdd553753f6c2e13
De-ma <dabuadas@uoguelph.ca>
2020-03-13 14:35:50 -0400

    stack views woot woot

--- Fremie Friends/NewFriendViewController.swift
+++ Fremie Friends/NewFriendViewController.swift
@@ -20,31 +20,30 @@ class NewFriendViewController: UIViewController, UITextFieldDelegate, UIPickerVi
         view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         self.navigationController?.isNavigationBarHidden = false
         self.navigationController?.navigationBar.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
-        self.navigationController?.navigationBar.topItem?.title = "Creating a friend"
+        self.navigationController?.navigationBar.topItem?.title = "Creating A Friend"
         
         let friendLabel: UILabel = {
             let label = UILabel()
-            label.text = "Name:"
-            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "Name: "
+            label.font = label.font.withSize(21)
             label.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
             return label
         }()
         
         friendName = {
             let textField = UITextField()
+            textField.placeholder = "Choose this wisely.."
+            textField.autocorrectionType = .no
             textField.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            textField.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-            textField.layer.borderWidth = 1
-            
-            textField.translatesAutoresizingMaskIntoConstraints = false
-
+            textField.addLine(position: .LINE_POSITION_BOTTOM, color: .black, width: 1)
             return textField
         }()
         
         let typeLabel: UILabel = {
             let label = UILabel()
-            label.text = "Fermentation Type:"
+            label.text = "Fermentation Type: "
             label.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            label.font = label.font.withSize(21)
             label.translatesAutoresizingMaskIntoConstraints = false
             return label
         }()
@@ -58,6 +57,7 @@ class NewFriendViewController: UIViewController, UITextFieldDelegate, UIPickerVi
         let dateLabel: UILabel = {
             let label = UILabel()
             label.text = "Birthday:"
+            label.font = label.font.withSize(21)
             label.translatesAutoresizingMaskIntoConstraints = false
             return label
         }()
@@ -68,7 +68,6 @@ class NewFriendViewController: UIViewController, UITextFieldDelegate, UIPickerVi
             dPicker.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
             dPicker.datePickerMode = .date
             dPicker.translatesAutoresizingMaskIntoConstraints = false
-
             return dPicker
         }()
         
@@ -103,55 +102,47 @@ class NewFriendViewController: UIViewController, UITextFieldDelegate, UIPickerVi
         friendType.dataSource = self
         friendType.delegate = self
         
-        view.addSubview(friendLabel)
-        view.addSubview(friendName)
-        view.addSubview(typeLabel)
-        view.addSubview(friendType)
-        view.addSubview(dateLabel)
-        view.addSubview(datePicker)
+        let mainStackView = UIStackView()
+        mainStackView.axis = .vertical
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.alignment = .top
+        mainStackView.distribution = .equalSpacing
+        mainStackView.spacing = 20
+        
+        
+        let friendLabelNameStackView = UIStackView()
+        friendLabelNameStackView.addArrangedSubview(friendLabel)
+        friendLabelNameStackView.addArrangedSubview(friendName)
+        friendLabelNameStackView.axis = .horizontal
+        friendLabelNameStackView.spacing = 10
+
+        mainStackView.addArrangedSubview(friendLabelNameStackView)
+        mainStackView.addArrangedSubview(typeLabel)
+        mainStackView.addArrangedSubview(friendType)
+        mainStackView.addArrangedSubview(dateLabel)
+        mainStackView.addArrangedSubview(datePicker)
+        view.addSubview(mainStackView)
         view.addSubview(addFriendButton)
         
-        /*
-         Would grouping them up into smalled subviews help this process?
-         */
+        mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+         mainStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+         mainStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
 
-        friendLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        friendLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        friendLabel.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2).isActive = true
-        friendLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        friendName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        friendName.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10).isActive = true
         friendName.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2).isActive = true
-        friendName.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        friendName.heightAnchor.constraint(equalTo: friendLabel.heightAnchor).isActive = true
+        friendType.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         
-        typeLabel.topAnchor.constraint(equalTo: friendLabel.bottomAnchor, constant: 10).isActive = true
-        typeLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        typeLabel.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2).isActive = true
-        typeLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        friendType.topAnchor.constraint(equalTo: typeLabel.bottomAnchor, constant: 0).isActive = true
-        friendType.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        friendType.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10).isActive = true
-        friendType.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
         friendType.heightAnchor.constraint(equalToConstant: 100).isActive = true
-
-        dateLabel.topAnchor.constraint(equalTo: friendType.bottomAnchor, constant: 10).isActive = true
-        dateLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        dateLabel.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2).isActive = true
-        dateLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
-        datePicker.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 0).isActive = true
-        datePicker.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        datePicker.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10).isActive = true
-        datePicker.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
-        datePicker.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        datePicker.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+
         
         addFriendButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
         addFriendButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        addFriendButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10).isActive = true
-        addFriendButton.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2).isActive = true
+        addFriendButton.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 20 ).isActive = true
         addFriendButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        addFriendButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
     }
     
     @objc func createNewFriend() {
@@ -189,6 +180,13 @@ class NewFriendViewController: UIViewController, UITextFieldDelegate, UIPickerVi
 }
 
 extension NewFriendViewController {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.isFirstResponder {
+            textField
+        }
+    }
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -204,5 +202,32 @@ extension NewFriendViewController {
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         selectedRow = friendOptions[row]
+    }
+}
+
+enum LINE_POSITION {
+    case LINE_POSITION_TOP
+    case LINE_POSITION_BOTTOM
+}
+
+extension UIView {
+    func addLine(position : LINE_POSITION, color: UIColor, width: Double) {
+        let lineView = UIView()
+        lineView.backgroundColor = color
+        lineView.translatesAutoresizingMaskIntoConstraints = false // This is important!
+        self.addSubview(lineView)
+
+        let metrics = ["width" : NSNumber(value: width)]
+        let views = ["lineView" : lineView]
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[lineView]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+
+        switch position {
+        case .LINE_POSITION_TOP:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[lineView(width)]", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        case .LINE_POSITION_BOTTOM:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[lineView(width)]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        }
     }
 }
